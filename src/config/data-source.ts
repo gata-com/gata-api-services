307db3e9323e81a7caac54a976a717fe88c4c165
rapleek <raflihaf47@gmail.com>
2025-10-03 16:15:22 +0700

    Update : google callback

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -12,7 +12,7 @@ const AppDataSource = new DataSource({
   host: process.env.DB_HOST || "localhost",
   port: parseInt(process.env.DB_PORT || "3306"),
   username: process.env.DB_USERNAME || "root",
-  password: process.env.DB_PASSWORD || "root",
+  password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "gataDB",
   synchronize: !isProduction,
   logging: process.env.DB_LOGGING === "true",

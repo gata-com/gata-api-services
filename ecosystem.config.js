c222d1089a788277e86012e8def70d832de08cd5
Ahmad Dwiky Zerro Dixxon <92775985+1wikii@users.noreply.github.com>
2025-11-15 22:02:51 +0700

    Update cwd for api-services in ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "api-services",
       script: "dist/index.js",     // hasil build TS ke JS
-      cwd: "/var/www/api-services",
+      cwd: "/var/www/gata-api-services",
       instances: "max",
       exec_mode: "cluster",
       watch: false,

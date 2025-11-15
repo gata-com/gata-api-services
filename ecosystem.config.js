84fd722274ab1b0579ac4f05485cc81b2767b05f
Ahmad Dwiky Zerro Dixxon <92775985+1wikii@users.noreply.github.com>
2025-11-16 02:18:17 +0700

    Rename app and update script path in config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   apps: [
     {
-      name: "api-services",
-      script: "dist/index.js",     // hasil build TS ke JS
+      name: "gata-api-services",
+      script: "dist/server.js",     // hasil build TS ke JS
       cwd: "/var/www/gata-api-services",
       instances: "max",
       exec_mode: "cluster",

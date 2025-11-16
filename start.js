90981c0fc1501cb1aecd8cff694b56045fdfc780
dwikidev <ahmad.121140044@student.itera.ac.id>
2025-11-16 16:46:04 +0700

    refactor: update path mappings in startjs

--- start.js
+++ start.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
-const path = require('path');
-const register = require('tsconfig-paths').register;
+const path = require("path");
+const register = require("tsconfig-paths").register;
 
 // Register the path mappings
 const cleanup = register({
-  baseUrl: __dirname + '/dist',
+  baseUrl: __dirname + "/dist",
   paths: {
-    '@/*': ['./*'],
-    '@/types/*': ['./types/*'],
-    '@/controllers/*': ['./controllers/*'],
-    '@/middleware/*': ['./middleware/*'],
-    '@/entities/*': ['./entities/*'],
-    '@/repositories/*': ['./repositories/*'],
-    '@/services/*': ['./services/*'],
-    '@/routes/*': ['./routes/*'],
-    '@/config/*': ['./config/*'],
-    '@/utils/*': ['./utils/*']
-  }
+    "@/*": ["./*"],
+    "@/types/*": ["./types/*"],
+    "@/controllers/*": ["./controllers/*"],
+    "@/middleware/*": ["./middleware/*"],
+    "@/entities/*": ["./entities/*"],
+    "@/repositories/*": ["./repositories/*"],
+    "@/services/*": ["./services/*"],
+    "@/routes/*": ["./routes/*"],
+    "@/config/*": ["./config/*"],
+    "@/utils/*": ["./utils/*"],
+  },
 });
 
-require('./dist/server.js');
+require("./dist/server.js");
